7b7c79566a1370173d991f7765625d3eb6fb2b48
jamesgotech <jameschibey@gmail.com>
2023-03-08 05:36:44 +0100

    auhentication problem

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -2,15 +2,30 @@ package middleware
 
 import (
 	"ecommerce/tokens"
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	// "net/url"
+
+	"github.com/gin-gonic/gin"
 )
 
 func Authentication() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		ClientLogin := c.Request.Header.Get("/user/login")
+		if ClientLogin == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "No authorization header provided"})
+			c.Abort()
+			return
+		}
+		ClientSignup := c.Request.Header.Get("/user/signup")
+		if ClientSignup == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "No authorization header provided"})
+			c.Abort()
+			return
+		}
 		ClientToken := c.Request.Header.Get("token")
 		if ClientToken == "" {
-			c.JSON(http.StatusInternalServerError, gin.H{"error":"No authorization header provided"})
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "No authorization header provided"})
 			c.Abort()
 			return
 		}
